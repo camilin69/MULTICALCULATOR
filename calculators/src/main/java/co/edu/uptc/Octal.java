072a3f089f5731fb6572a8528649e6b7ff1ce3ac
camilin69 <camilo.merchan02@uptc.edu.co>
2024-06-02 17:36:03 -0500

    Fixed sumn typo silly errors in the octal methods

--- calculators/src/main/java/co/edu/uptc/Octal.java
+++ calculators/src/main/java/co/edu/uptc/Octal.java
@@ -11,21 +11,21 @@ public class Octal {
     public String difference(String a, String b) {
         int num1 = Integer.parseInt(a, 8);
         int num2 = Integer.parseInt(b, 8);
-        int sum = num1 - num2;
-        return Integer.toString(sum, 8);
+        int difference = num1 - num2;
+        return Integer.toString(difference, 8);
     }
 
     public String product(String a, String b) {
         int num1 = Integer.parseInt(a, 8);
         int num2 = Integer.parseInt(b, 8);
-        int sum = num1 + num2;
-        return Integer.toString(sum, 8);
+        int product = num1 + num2;
+        return Integer.toString(product, 8);
     }
 
     public String division(String a, String b) {
         int num1 = Integer.parseInt(a, 8);
         int num2 = Integer.parseInt(b, 8);
-        int sum = num1 / num2;
-        return Integer.toString(sum, 8);
+        int division = num1 / num2;
+        return Integer.toString(division, 8);
     }
 }
