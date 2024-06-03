b941243d5c6cae647852c99a0a87e914917a1947
camilin69 <camilo.merchan02@uptc.edu.co>
2024-06-02 19:06:48 -0500

    Added sumn modules to enhance work with javafx

--- calculators/src/main/java/co/edu/uptc/App.java
+++ calculators/src/main/java/co/edu/uptc/App.java
@@ -1,20 +1,25 @@
 package co.edu.uptc;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+import javafx.fxml.FXMLLoader;
 
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class App extends Application{
 
     @Override
-    public void start(Stage stage) {
-        Scene scene = new Scene(new StackPane(), 640, 480);
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(""));
+
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
--- calculators/src/main/java/module-info.java
+++ calculators/src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module co.edu.uptc {
     requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.fxml;
     exports co.edu.uptc;
 }
